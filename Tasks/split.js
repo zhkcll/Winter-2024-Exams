7d1090df983c6d260177ea31c19df65b107d102f
zhkcll <143878947+zhkcll@users.noreply.github.com>
2024-01-15 00:00:00 +0200

    Update split.js

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,9 +1,10 @@
 // Split an array into two parts
 
-let split_Array = (index, array) => {
-  begin = array.slice(0, index);
-  const len = array.length;
-  array = array.slice(index, len);
+'use strict';
+
+ const splitArray = (index, array) => {
+   res=[array.slice(0, index), array,slice(index, array.length)];
+   
   return [begin, array];
 };
 
