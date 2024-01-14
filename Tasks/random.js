6487a469a785cec31d48353df9d11791f9a3a528
zhkcll <143878947+zhkcll@users.noreply.github.com>
2024-01-14 23:24:36 +0200

    Update random.js

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,6 +1,8 @@
 // Generate random integer value in given range
 
-Random = (min, max) => {
+'use strict'
+
+const Random = (min, max) => {
   if (max === undefined) {
     max = min;
     return Math.floor(Math.random() * (max + 1));
