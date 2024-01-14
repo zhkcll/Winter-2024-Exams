87f893689c2976bd234a628a8fb220a1fe96d657
zhkcll <143878947+zhkcll@users.noreply.github.com>
2024-01-14 23:36:26 +0200

    Update day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,11 +1,13 @@
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+'use srict';
+
+const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 _parse_day_ = (s) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < week.length; i++) {
+    if (s.startsWith(week[i].toLowerCase())) {
       return i + 1;
     }
   }
